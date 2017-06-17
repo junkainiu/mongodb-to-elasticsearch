9113c277bb06e54cfc05766295de809e999ff867
Leo Niu <leo.niu@netis.com.cn>
2017-06-17 12:07:15 +0800

    fix name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import pipetrans
 
 setup(
-    name='mongo_to_elasticsearch',
+    name='mongodb-to-elasticsearch',
     version='0.1',
     description='Transform mongo aggregate pipeline to elasticsearch query',
     url='https://github.com/junkainiu/mongo_to_elasticsearch',
